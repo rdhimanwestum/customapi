b5741398a6bdb41f1469ff8a4b4f2d82f15647fb
Rohit Dhiman <rdhiman@westum.com>
2019-03-01 11:47:29 +0200

    Removed Unwanted Code

--- Westum/CustApi/Console/GenerateFakeData.php
+++ Westum/CustApi/Console/GenerateFakeData.php
@@ -7,7 +7,7 @@
  */
 namespace Westum\CustApi\Console;
 
-use Magento\Setup\Exception;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
