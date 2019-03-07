559d4247c3698b885a641902cacec673cde668fa
root <rdhimanwestum>
2019-03-07 13:18:14 +0200

    Import From ElasticData Crons Structure

--- Westum/CustApi/Cron/Customer.php
+++ Westum/CustApi/Cron/Customer.php
@@ -5,3 +5,46 @@
  * Date: 3/6/19
  * Time: 11:08 AM
  */
+
+namespace Westum\CustApi\Cron;
+
+use \Psr\Log\LoggerInterface;
+
+
+class Customer
+{
+    /**
+     * @var \Magento\AdvancedSearch\Model\Client\ClientResolver
+     */
+    protected $clientResolver;
+
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    /**
+     * @var \Magento\Elasticsearch\Model\Adapter\Elasticsearch
+     */
+    protected $adapterElasticsearch;
+
+    protected $logger;
+
+    public function __construct(
+        LoggerInterface $logger,
+        \Magento\AdvancedSearch\Model\Client\ClientResolver $clientResolver,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Elasticsearch\Model\Adapter\Elasticsearch $adapterElasticsearch
+    )
+    {
+        $this->logger = $logger;
+        $this->clientResolver = $clientResolver;
+        $this->scopeConfig = $scopeConfig;
+        $this->adapterElasticsearch = $adapterElasticsearch;
+    }
+
+    public function execute() {
+        $this->logger->info('Customer Cron Works');
+        echo 'Customer';
+    }
+}
--- Westum/CustApi/Cron/Order.php
+++ Westum/CustApi/Cron/Order.php
@@ -5,3 +5,29 @@
  * Date: 3/6/19
  * Time: 11:08 AM
  */
+
+namespace Westum\CustApi\Cron;
+
+use \Psr\Log\LoggerInterface;
+
+
+class Order
+{
+
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Write to system.log
+     *
+     * @return void
+     */
+
+    public function execute() {
+        $this->logger->info('Order Cron Works');
+        echo 'Order';
+    }
+}
